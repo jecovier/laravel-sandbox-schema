8ff6e44ce8304d0e6ad916b444d81b9ebc2000d4
Jose Espinoza Conforme <jecovier@gmail.com>
2020-02-21 01:20:25 -0500

    fix: name of configuration file

--- src/SandboxSchemaServiceProvider.php
+++ src/SandboxSchemaServiceProvider.php
@@ -17,7 +17,7 @@ class SandboxSchemaServiceProvider extends ServiceProvider
 
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
         $this->publishes([
-            __DIR__ . '/config.php' => config_path('sanboxschema.php'),
+            __DIR__ . '/config.php' => config_path('sandboxschema.php'),
         ], 'config');
     }
 
